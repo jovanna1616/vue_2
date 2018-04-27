116bb0da4d4c157242f0097b32312305fbb881b1
jovanna1616 <jovana.r@vivifyideas.com>
2018-04-27 21:31:11 +0200

    6. zadatak.

--- src/pages/AppProducts.vue
+++ src/pages/AppProducts.vue
@@ -1,19 +1,22 @@
 <template>
-  <table>
-      <thead>
-        <th>Product ID</th>
-        <th>Product title</th>
-        <th>Product quantity</th>
-        <th>&nbsp;</th>
-      </thead>
-      <tbody>
-        <tr v-for="product in products" :key="product.id">
-          <td>{{ product.id }}</td>
-          <td>{{ product.title }}</td>
-          <td>{{ product.quantity }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div>
+    <input v-model="searchTerm" type="text" class="form-control" placeholder="search">
+    <table>
+        <thead>
+          <th>Product ID</th>
+          <th>Product title</th>
+          <th>Product quantity</th>
+          <th>&nbsp;</th>
+        </thead>
+        <tbody>
+          <tr v-for="product in filterProducts" :key="product.id">
+            <td>{{ product.id }}</td>
+            <td>{{ product.title }}</td>
+            <td>{{ product.quantity }}</td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
 </template>
 
 <script>
@@ -23,9 +26,17 @@ export default {
   name: 'AppProducts',
   data() {
     return {
-      products: productService.list()
+      products: productService.list(),
+      searchTerm: ''
     }
   },
+  computed: {
+    filterProducts() {
+      return this.products.filter((product) => {
+        return product.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+      });
+    }
+  }
 }
 </script>
 
