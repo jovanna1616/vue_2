4bbbcd4dd87c4da7c80f1cad4c530c9466e3d671
jovanna1616 <jovana.r@vivifyideas.com>
2018-04-27 19:24:19 +0200

    2. zadatak.

--- src/pages/AppCustomers.vue
+++ src/pages/AppCustomers.vue
@@ -1,10 +1,41 @@
 <template>
-  <div>app customers works!</div>
+  <div>
+    <table>
+      <thead>
+        <th>Customer ID</th>
+        <th>First name</th>
+        <th>Last name</th>
+        <th>Email</th>
+        <th>&nbsp;</th>
+      </thead>
+      <tbody>
+        <tr v-for="(customer, key) in customers" :key="key">
+          <td>{{ customer.id }}</td>
+          <td>{{ customer.firstName }}</td>
+          <td>{{ customer.lastName }}</td>
+          <td>{{ customer.email }}</td>
+          <td><button @click="removeCustomer(customer)">Remove customer</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {
+import { customerService } from '../services/CustomerService';
 
+export default {
+  name: 'AppCustomers',
+  data() {
+    return {
+      customers: customerService.list()
+    }
+  },
+  methods: {
+    removeCustomer(customer) {
+      customerService.removeCustomer(customer);
+    }
+  }
 }
 </script>
 
---
+++ src/services/CustomerService.js
@@ -0,0 +1,18 @@
+const customers = [
+    {id: 1, firstName: 'John', lastName: 'Doe', email: 'john@doe.com', productsList: []},
+    {id: 2, firstName: 'Joan', lastName: 'Doe', email: 'joan@doe.com', productsList: []},
+    {id: 3, firstName: 'Tom', lastName: 'White', email: 'tom@white.com', productsList: []},
+    {id: 4, firstName: 'Jerry', lastName: 'Black', email: 'jerry@black.com', productsList: []}
+];
+
+export default class CustomerService{
+    list() {
+      return customers
+    }
+
+    removeCustomer(customer) {
+      customers.splice(customers.indexOf(customer));
+    }
+}
+    
+export const customerService = new CustomerService()
